e8c65a561dddf7ba9d48c10fa397d4eba233c9c7
codingarmy45 <135152967+codingarmy45@users.noreply.github.com>
2025-12-15 17:06:57 +0000

    01_counter

---
+++ src/components/01_Counter/Counter.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [counter, setCounter] = useState(0)
+  return (
+    <div>
+      <button onClick={()=>setCounter(counter+1)}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={()=>setCounter(counter-1)}>-</button>
+    </div>
+  )
+}
+
+export default Counter
